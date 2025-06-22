163580527a9da3542b35faba22ef77e18eb8f299
flo-flo-lenormand <florent.lenormand@icloud.com>
2025-06-22 08:59:17 +0200

    Create index.js

---
+++ pages/index.js
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <div style={{ textAlign: "center", marginTop: "100px" }}>
+      <h1>Hello Flo 👋</h1>
+      <p>This is your first custom page. Vercel rocks!</p>
+    </div>
+  )
+}
