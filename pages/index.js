df96b3b382832b90fc3b0e4d3fa0ca4caf548b70
flo-flo-lenormand <florent.lenormand@icloud.com>
2025-06-22 09:05:25 +0200

    Delete pages directory

--- pages/index.js
+++
@@ -1,8 +0,0 @@
-export default function Home() {
-  return (
-    <div style={{ textAlign: "center", marginTop: "100px" }}>
-      <h1>Hello Flo 👋</h1>
-      <p>This is your first custom page. Vercel rocks!</p>
-    </div>
-  )
-}
